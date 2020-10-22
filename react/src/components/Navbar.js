4a4942bc369848f65b948776aa4fddab11dc4ee5
davidaxel121 <davidaxel121@gmail.com>
2020-10-21 21:11:09 -0500

    Adding paddings and resized components

--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -9,20 +9,20 @@ const MyNavbar = () => {
 	return (
 		<div className="navbar-container">
 			<Navbar className="navbar" fixed="top" expand="lg">
-				<Navbar.Brand><Link className="navbar-link" to="/">Navbar</Link></Navbar.Brand>
+				<Navbar.Brand><Link className="navbar-link" to="/">Logo</Link></Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ml-auto text-center">
 						{/* <Nav className="nav-link"><Link className="navbar-link" to="/">Home</Link></Nav> */}
 						{/* <Nav className="nav-link"><Link className="navbar-link" to="/anotherpage">Another Page</Link></Nav> */}
-						<Nav className="nav-link">Foto</Nav>
-						<Nav className="nav-link">Sobre nosotros</Nav>
-						<Nav className="nav-link">Servicios</Nav>
-						<Nav className="nav-link">Instalaciones</Nav>
-						<Nav className="nav-link">Ubicacion</Nav>
-						<Nav className="nav-link">Contacto</Nav>
-						<Nav className="nav-link">Footer</Nav>
+						{/* <Nav className="nav-link">Foto</Nav> */}
+						<Nav className="nav-link"><Link className="navbar-link">Sobre nosotros</Link></Nav>
+						<Nav className="nav-link"><Link className="navbar-link">Servicios</Link></Nav>
+						<Nav className="nav-link"><Link className="navbar-link">Instalaciones</Link></Nav>
+						<Nav className="nav-link"><Link className="navbar-link">Ubicacion</Link></Nav>
+						<Nav className="nav-link"><Link className="navbar-link">Contacto</Link></Nav>
+						{/* <Nav className="nav-link">Footer</Nav> */}
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
